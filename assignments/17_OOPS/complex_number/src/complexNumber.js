a6c9106ef81105ac108f896a1802f178e18af02b
devdatta <183721457+devdattadhage@users.noreply.github.com>
2025-02-01 17:03:44 +0530

    improved

--- assignments/17_OOPS/complex_number/src/complexNumber.js
+++ assignments/17_OOPS/complex_number/src/complexNumber.js
@@ -6,7 +6,31 @@ export class ComplexNumber {
     this.#imaginary = imaginary;
   }
 
-  visualizeComplexNum() {
+  visualizeHelper() {}
+
+  visualize() {
     return `${this.#real} + ${this.#imaginary}i`;
+  }
+
+  add(cn) {
+    return new ComplexNumber(
+      this.#real + cn.#real,
+      this.#imaginary + cn.#imaginary
+    );
+  }
+
+  sub(cn) {
+    return new ComplexNumber(
+      this.#real - cn.#real,
+      this.#imaginary - cn.#imaginary
+    );
+  }
+
+  addMultiple(...cns) {
+    return cns.reduce((init, cn) => init.add(cn), this);
+  }
+
+  subMultiple(...cns) {
+    return cns.reduce((init, cn) => init.sub(cn), this);
   }
 }
--- assignments/17_OOPS/complex_number/test/complexNumber_test.js
+++ assignments/17_OOPS/complex_number/test/complexNumber_test.js
@@ -1,27 +1,112 @@
 import { describe, it } from "jsr:@std/testing/bdd";
-import { assert, assertEquals, assertFalse } from "jsr:@std/assert";
+import {
+  assertEquals,
+  assertInstanceOf,
+  assertNotInstanceOf,
+} from "jsr:@std/assert";
 import { ComplexNumber } from "../src/complexNumber.js";
 
 describe("class ComplexNumber", () => {
   describe("instance", () => {
     it("should be the instance of the class", () => {
       const c1 = new ComplexNumber(2, 3);
-      assert(c1 instanceof ComplexNumber);
+      assertInstanceOf(c1, ComplexNumber);
     });
 
     it("should not be the instance of the class", () => {
       const c1 = new ComplexNumber(2, 3);
       const c2 = new String();
-      assertFalse(c2 instanceof ComplexNumber);
-      assertFalse(c1 instanceof String);
-      assertFalse("c3" instanceof ComplexNumber);
+      assertNotInstanceOf(c2, ComplexNumber);
+      assertNotInstanceOf(c1, String);
+      assertNotInstanceOf("c3", ComplexNumber);
     });
   });
 
-  describe("visualizeComplexNum()", () => {
+  describe("visualize()", () => {
     it("should return in complex number format", () => {
-      assertEquals(new ComplexNumber(2, 3).visualizeComplexNum(), "2 + 3i");
-      assertEquals(new ComplexNumber(4, 5).visualizeComplexNum(), "4 + 5i");
+      assertEquals(new ComplexNumber(2, 3).visualize(), "2 + 3i");
+    });
+  });
+
+  describe("add()", () => {
+    it("should add two complex number", () => {
+      const c1 = new ComplexNumber(2, 3);
+      const c2 = new ComplexNumber(4, 5);
+      assertEquals(c1.add(c2).visualize(), "6 + 8i");
+    });
+
+    it("should add two complex number when the numbers are negative", () => {
+      const c1 = new ComplexNumber(-2, 6);
+      const c2 = new ComplexNumber(4, -3);
+      assertEquals(c1.add(c2).visualize(), "2 + 3i");
+    });
+  });
+
+  describe("sub()", () => {
+    it("should sub two complex number", () => {
+      const c1 = new ComplexNumber(2, 3);
+      const c2 = new ComplexNumber(4, 1);
+      assertEquals(c1.sub(c2).visualize(), "-2 + 2i");
+    });
+
+    it("should sub two complex number when the numbers are negative", () => {
+      const c1 = new ComplexNumber(-2, 6);
+      const c2 = new ComplexNumber(4, -3);
+      assertEquals(c1.sub(c2).visualize(), "-6 + 9i");
+    });
+  });
+
+  describe("addMultiple()", () => {
+    it("should return the same when only single complex number is given", () => {
+      const c1 = new ComplexNumber(2, 3);
+      assertEquals(c1.addMultiple().visualize(), "2 + 3i");
+    });
+
+    it("should add two complex number", () => {
+      const c1 = new ComplexNumber(2, 3);
+      const c2 = new ComplexNumber(4, 5);
+      assertEquals(c1.addMultiple(c2).visualize(), "6 + 8i");
+    });
+
+    it("should add multiple complex number", () => {
+      const c1 = new ComplexNumber(2, 3);
+      const c2 = new ComplexNumber(4, 5);
+      const c3 = new ComplexNumber(6, 7);
+      assertEquals(c1.addMultiple(c2, c3).visualize(), "12 + 15i");
+    });
+
+    it("should add multiple complex number when the numbers are negative", () => {
+      const c1 = new ComplexNumber(-2, 3);
+      const c2 = new ComplexNumber(4, -5);
+      const c3 = new ComplexNumber(-6, 7);
+      assertEquals(c1.addMultiple(c2, c3).visualize(), "-4 + 5i");
+    });
+  });
+
+  describe("subMultiple()", () => {
+    it("should return the same when only single complex number is given", () => {
+      const c1 = new ComplexNumber(2, 3);
+      assertEquals(c1.subMultiple().visualize(), "2 + 3i");
+    });
+
+    it("should sub two complex number", () => {
+      const c1 = new ComplexNumber(2, 3);
+      const c2 = new ComplexNumber(4, 5);
+      assertEquals(c1.subMultiple(c2).visualize(), "-2 + -2i");
+    });
+
+    it("should sub multiple complex number", () => {
+      const c1 = new ComplexNumber(2, 3);
+      const c2 = new ComplexNumber(4, 5);
+      const c3 = new ComplexNumber(6, 7);
+      assertEquals(c1.subMultiple(c2, c3).visualize(), "-8 + -9i");
+    });
+
+    it("should sub multiple complex number when the numbers are negative", () => {
+      const c1 = new ComplexNumber(-2, 3);
+      const c2 = new ComplexNumber(4, -5);
+      const c3 = new ComplexNumber(-7, 6);
+      assertEquals(c1.subMultiple(c2, c3).visualize(), "1 + 2i");
     });
   });
 });
