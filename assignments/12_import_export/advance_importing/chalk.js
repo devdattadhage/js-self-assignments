59f4bdec72f178f313cb3f063f3946f642bf794f
devdatta <183721457+devdattadhage@users.noreply.github.com>
2025-03-18 23:50:58 +0530

    Added advance_importing directory

---
+++ assignments/12_import_export/advance_importing/chalk.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+// console.log(chalk.bgRed.yellow.bold("hello world"));
+console.log(chalk.yellow.underline("hello world"));
+// console.log(chalk.hex("dda15e")("hello world"));
+// console.log(chalk.rgb(123, 45, 67)("hello world"));
