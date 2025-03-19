638165a2b5a5d4d25eeee752605456b24ccd26ed
devdatta <183721457+devdattadhage@users.noreply.github.com>
2025-03-19 23:31:49 +0530

    Handle error gracefully

--- assignments/23_networking/index.js
+++ assignments/23_networking/index.js
@@ -1,41 +1,55 @@
 import _ from "npm:lodash";
+
 const listener = Deno.listen({ port: 8000 });
 let connections = [];
 
-async function handleConnection(conn, clientId) {
-  try {
-    const buffer = new Uint8Array(1024);
+async function handleRequest(conn, buffer, clientId, decoder, encoder) {
+  while (true) {
     const byteCount = await conn.read(buffer);
-    const decoder = new TextDecoder();
-    const encoder = new TextEncoder();
-    const message = decoder.decode(buffer.slice(0, byteCount));
 
+    if (byteCount === null) {
+      console.log(
+        `Client ${clientId} disconnected. Available Connections ${
+          connections.length - 1
+        }`
+      );
+      connections = _.without(connections, conn);
+      break;
+    }
+
+    const message = decoder.decode(buffer.slice(0, byteCount)).trim();
     console.log(`Client Id (${clientId}) : ${message}`);
+    await conn.write(encoder.encode(`Request Received: ${message}\n`));
+  }
+}
 
-    await conn.write(encoder.encode(`Message Sent:${message}`));
+async function handleConnection(conn, clientId) {
+  const buffer = new Uint8Array(1024);
+  const decoder = new TextDecoder();
+  const encoder = new TextEncoder();
+
+  try {
+    await handleRequest(conn, buffer, clientId, decoder, encoder);
   } catch (e) {
-    connections = _.without(connections, conn);
-    console.log("Disconnected. Available Connection", connections.length);
+    console.error(`Error with client ${clientId}:`, e);
+  } finally {
+    conn.close();
   }
 }
 
 const main = async () => {
   let connectionId = 0;
-  setInterval(async () => {
-    for await (const conn of connections) {
-      try {
-        handleConnection(conn, connectionId);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  }, 200);
 
   while (true) {
     const conn = await listener.accept();
     connectionId += 1;
     connections.push(conn);
-    console.log("Connected. Available Connection", connections.length);
+    console.log(
+      `Client ${connectionId} connected. Total Connections:`,
+      connections.length
+    );
+
+    handleConnection(conn, connectionId);
   }
 };
 
